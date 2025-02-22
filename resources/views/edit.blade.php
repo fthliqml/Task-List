47942d4b56094d662d3b9386ddc8bf5d84f2a755
Muhammad Fatihul Iqmal <fatihuliqmalzzz@gmail.com>
2025-02-22 18:02:08 +0800

    feat: add TaskRequest for better validation and adding mass assignment

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -12,7 +12,7 @@
 @endsection
 
 @section('content')
-    <form method="POST" action="{{ route('tasks.update', ['id' => $task->id]) }}">
+    <form method="POST" action="{{ route('tasks.update', ['task' => $task->id]) }}">
         @csrf
         {{-- Change POST method to PUT --}}
         @method('PUT')
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,8 @@
 <?php
 
+use App\Http\Requests\TaskRequest;
 use App\Models\Task;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
-
 
 Route::get('/', function () {
     return redirect()->route("tasks.index");
@@ -21,64 +19,30 @@ Route::get('/tasks', function () {
 // Use View if we doesn't need sending data to views
 Route::view('/tasks/create', 'create')->name("tasks.create");
 
-
-Route::get("/tasks/{id}", function ($id) {
-    return view('show', ["task" => Task::findOrFail($id)]);
+Route::get("/tasks/{task}", function (Task $task) {
+    return view('show', compact("task")); // compact : Automatically create array asosiatif
 })->name("tasks.show");
 
-Route::get("/tasks/{id}/edit", function ($id) {
+Route::get("/tasks/{task}/edit", function (Task $task) {
     return view("edit", [
-        "task" => Task::findOrFail($id)
+        "task" => $task // automatically find data by id
     ]);
 })->name("tasks.edit");
 
-Route::put('/tasks/{id}', function ($id, Request $request) {
-    $data = $request->validate([
-        'title' => 'required|max:255',
-        'description' => 'required',
-        'long_description' => 'required'
-    ], [
-        'title.required' => "Judul tidak boleh kosong!",
-        'description.required' => "Deskripsi tidak boleh kosong!",
-        'long_description.required' => "Deskripsi panjang tidak boleh kosong!"
-    ]);
+Route::post('/tasks', function (TaskRequest $request) {
+    // create new task and store it to database
+    $task = Task::create($request->validated());
 
-    // find data
-    $task = Task::findOrFail($id);
-    $task->title = $data['title'];
-    $task->description = $data['description'];
-    $task->long_description = $data['long_description'];
-
-    // edit data
-    $task->save();
-
-    return redirect()->route("tasks.show", ['id' => $task->id])
-        ->with('success', 'Task edited successfully!'); // flash message, accept in view using session()
-})->name('tasks.update');
-
-Route::post('/tasks', function (Request $request) {
-    $data = $request->validate([
-        'title' => 'required|max:255',
-        'description' => 'required',
-        'long_description' => 'required'
-    ], [
-        'title.required' => "Judul tidak boleh kosong!",
-        'description.required' => "Deskripsi tidak boleh kosong!",
-        'long_description.required' => "Deskripsi panjang tidak boleh kosong!"
-    ]);
-
-    // create new task model
-    $task = new Task;
-    $task->title = $data['title'];
-    $task->description = $data['description'];
-    $task->long_description = $data['long_description'];
-
-    // insert data
-    $task->save();
-
-    return redirect()->route("tasks.show", ['id' => $task->id])
+    return redirect()->route("tasks.show", ['task' => $task->id])
         ->with('success', 'Task created successfully!');
 })->name('tasks.store');
+
+Route::put('/tasks/{task}', function (Task $task, TaskRequest $request) {
+    $task->update($request->validated());
+
+    return redirect()->route("tasks.show", ['task' => $task->id])
+        ->with('success', 'Task edited successfully!'); // flash message, accept in view using session()
+})->name('tasks.update');
 
 // If user try to access route that doesn't exist
 Route::fallback(function () {
